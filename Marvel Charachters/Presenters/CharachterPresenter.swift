571615fff8d67eca2b7dba471e066966a24d06f9
h33h <beolki102@gmail.com>
2021-10-21 00:50:20 +0300

    Add new request methods

--- Marvel Charachters/Presenters/CharachterPresenter.swift
+++ Marvel Charachters/Presenters/CharachterPresenter.swift
@@ -24,19 +24,89 @@ class CharachterPresenter {
         charachterService.getCharachters(offset: offset, limit: limit) { [weak self] data, response, _ in
             guard let data = data else { return }
             let jsonDecoder = JSONDecoder()
-            let response = try? jsonDecoder.decode(CharachterResponse.self, from: data)
-            if let response = response {
-            self?.charachterDelegate?.displayAllCharachters(charachters: response.data.results)
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displayAllCharachters(charachters: response)
             }
         }
     }
-    func getSelectedCharachter(nameStartsWith: String) {
-        charachterService.getCharachterByName(nameStartsWith: nameStartsWith) { [weak self] data, response, _ in
+    func getCharachtersByNameStartsWith(nameStartsWith: String) {
+        charachterService.getCharachtersByName(nameStartsWith: nameStartsWith) { [weak self] data, response, _ in
             guard let data = data else { return }
             let jsonDecoder = JSONDecoder()
-            let response = try? jsonDecoder.decode(CharachterResponse.self, from: data)
-            if let response = response {
-                self?.charachterDelegate?.displaySelectedCharachter(charachters: response.data.results)
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
+            }
+        }
+    }
+    func getCharachtersByFullName(name: String) {
+        charachterService.getCharachterByFullName(name: name) { [weak self] data, response, _ in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
+            }
+        }
+    }
+    func getCharachtersByFilter(filter: CharachterFilter) {
+        charachterService.getCharachtersByFilter(filter: filter) { [weak self] data, response, _ in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
+            }
+        }
+    }
+    func getCharachtersByModifiedSince(date: String) {
+        charachterService.getCharachtersModifiedSince(date: date) { [weak self] data, response, _ in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
+            }
+        }
+    }
+    func getCharachtersByComicsID(id: Int) {
+        charachterService.getCharachtersByComicsID(id: id) { [weak self] data, response, _ in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
+            }
+        }
+    }
+    func getCharachtersBySeriesID(id: Int) {
+        charachterService.getCharachtersBySeriesID(id: id) { [weak self] data, response, _ in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
+            }
+        }
+    }
+    func getCharachtersByStoriesID(id: Int) {
+        charachterService.getCharachtersByStoriesID(id: id) { [weak self] data, response, _ in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
+            }
+        }
+    }
+    func getCharachtersByEventsID(id: Int) {
+        charachterService.getCharachtersByEventsID(id: id) { [weak self] data, response, _ in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            let response = try? jsonDecoder.decode(CharacterDataWrapper.self, from: data)
+            if let response = response?.data?.results {
+                self?.charachterDelegate?.displaySelectedCharachter(charachters: response)
             }
         }
     }
